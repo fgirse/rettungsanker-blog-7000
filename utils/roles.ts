6feebdc014e464b113243736b484c8e1b5c275f7
fgirse <fgirs@bluewin.ch>
2026-01-20 00:29:51 +0100

    delete role.ts

--- utils/roles.ts
+++
@@ -1,7 +0,0 @@
-import { auth } from '@clerk/nextjs/server'
-
-export const checkRole = async (role: 'admin' | 'user') => {
-  const { sessionClaims } = await auth()
-
-  return (sessionClaims?.metadata as { role?: string })?.role === role
-}
