d023aa8a9b68262e1f758610992ec66f921a19e2
fgirse <fgirs@bluewin.ch>
2026-02-03 18:14:06 +0100

    fix tailwind issues

--- app/(frontend)/(root)/layout.tsx
+++ app/(frontend)/(root)/layout.tsx
@@ -18,7 +18,7 @@ export default async function HomeLayout({
       <UserProvider user={user}>
          <div className="relative">
             <div 
-               className="absolute inset-x-0 top-0 w-full h-[900px] sm:h-[950px] md:h-[1050px] lg:h-[1400px] -z-10 pointer-events-none bg-[url('/Assets/Img/lighthouse.webp')] bg-cover bg-no-repeat bg-center lg:bg-[url('/Assets/Svg/5555.svg')] lg:bg-contain lg:bg-no-repeat lg:bg-center opacity-100"
+               className="absolute inset-x-0 top-0 w-full h-225 sm:h-237.5 md:h-262.5 lg:h-350 -z-10 pointer-events-none bg-[url('/Assets/Img/lighthouse.webp')] bg-cover bg-no-repeat bg-center lg:bg-[url('/Assets/Svg/5555.svg')] lg:bg-contain lg:bg-no-repeat lg:bg-center opacity-100"
             ></div>
             <Navbar />
             <main>
